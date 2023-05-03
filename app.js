37f1ee26a600b96feab938085e08585c8def855a
deyixtan <deyixtan@gmail.com>
2023-05-03 16:48:20 +0800

    Fix backend typo

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
 import express from "express";
 import { WebSocketServer } from "ws";
-import { HTTP_PORT, WSS_PORT } from "./config.js";
+import { HTTP_PORT, WS_PORT } from "./config.js";
 
 const app = express();
-const wss = new WebSocketServer({ port: WSS_PORT });
+const wsServer = new WebSocketServer({ port: WS_PORT });
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
@@ -13,12 +13,12 @@ app.all("/", (req, res) => {
   const { body, headers, params, url } = req;
   const response = { body, headers, params, url };
   console.log(response);
-  wss.clients.forEach((client) => client.send(JSON.stringify(response)));
+  wsServer.clients.forEach((client) => client.send(JSON.stringify(response)));
   res.status(200).send(response);
 });
 
 app.listen(HTTP_PORT, () => {
   console.log(
-    `webhook running on port ${HTTP_PORT} (HTTP) and ${WSS_PORT} (WSS)`
+    `webhook running on port ${HTTP_PORT} (HTTP) and ${WS_PORT} (WS)`
   );
 });
--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 import "dotenv/config";
 
 export const HTTP_PORT = process.env.HTTP_PORT || 8000;
-export const WSS_PORT = process.env.WS_PORT || 8001;
+export const WS_PORT = process.env.WS_PORT || 8001;
